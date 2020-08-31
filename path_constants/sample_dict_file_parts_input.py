71ec873603ba937c815563d0163d46a40eb91030
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-31 14:33:00 +0200

    completing parts sample dict with signal paths

--- path_constants/sample_dict_file_parts_input.py
+++ path_constants/sample_dict_file_parts_input.py
@@ -7,7 +7,15 @@ update_dict = {
 
 	'sample_dir' : {
 		'qcdSig': 'qcd_sqrtshatTeV_13TeV_PU40_parts',
-		'qcdSigSingle': 'qcd_sqrtshatTeV_13TeV_PU40'
+		'qcdSigSingle': 'qcd_sqrtshatTeV_13TeV_PU40',
+                'GtoWW15na': 'RSGraviton_WW_NARROW_13TeV_PU40_1.5TeV_parts',
+                'GtoWW15br': 'RSGraviton_WW_BROAD_13TeV_PU40_1.5TeV_parts',
+                'GtoWW25na': 'RSGraviton_WW_NARROW_13TeV_PU40_2.5TeV_parts',
+                'GtoWW25br': 'RSGraviton_WW_BROAD_13TeV_PU40_2.5TeV_parts',
+                'GtoWW35na': 'RSGraviton_WW_NARROW_13TeV_PU40_3.5TeV_parts',
+                'GtoWW35br': 'RSGraviton_WW_BROAD_13TeV_PU40_3.5TeV_parts',
+                'GtoWW45na': 'RSGraviton_WW_NARROW_13TeV_PU40_4.5TeV_parts',
+                'GtoWW45br': 'RSGraviton_WW_BROAD_13TeV_PU40_4.5TeV_parts',
 	},
 }
 
--- path_constants/sample_dict_file_parts_reco.py
+++ path_constants/sample_dict_file_parts_reco.py
@@ -8,8 +8,17 @@ update_dict = {
         'base_dir' : '/eos/home-k/kiwoznia/dev/autoencoder_for_anomaly/convolutional_VAE/results',
 
 	'sample_dir' : {
-		'qcdSig': 'qcd_sqrtshatTeV_13TeV_PU40_parts',
-		'qcdSigSingle': 'qcd_sqrtshatTeV_13TeV_PU40'
+		'qcdSigReco': 'qcd_sqrtshatTeV_13TeV_PU40_parts',
+                'GtoWW15naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_1.5TeV',
+                'GtoWW15brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_1.5TeV',
+                'GtoWW25naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_2.5TeV',
+                'GtoWW25brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_2.5TeV',
+                'GtoWW30naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_3.0TeV',
+                'GtoWW30brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.0TeV',
+                'GtoWW35naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_3.5TeV',
+                'GtoWW35brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.5TeV',
+                'GtoWW45naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_4.5TeV',
+                'GtoWW45brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_4.5TeV',
 	},
 }
 
