929a2889744f4968568529c1256dee5f34a7ce6a
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-31 15:54:44 +0200

    deepcopy of default path_dict in parts_input and parts_reco dictionaries, otherwise not usable in same script (e.g. prediction using input and result path dictionaries)

--- path_constants/sample_dict_file_parts_reco.py
+++ path_constants/sample_dict_file_parts_reco.py
@@ -9,16 +9,16 @@ update_dict = {
 
 	'sample_dir' : {
 		'qcdSigReco': 'qcd_sqrtshatTeV_13TeV_PU40_parts',
-                'GtoWW15naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_1.5TeV',
-                'GtoWW15brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_1.5TeV',
-                'GtoWW25naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_2.5TeV',
-                'GtoWW25brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_2.5TeV',
-                'GtoWW30naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_3.0TeV',
-                'GtoWW30brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.0TeV',
-                'GtoWW35naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_3.5TeV',
-                'GtoWW35brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.5TeV',
-                'GtoWW45naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_4.5TeV',
-                'GtoWW45brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_4.5TeV',
+                'GtoWW15naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_1.5TeV_parts',
+                'GtoWW15brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_1.5TeV_parts',
+                'GtoWW25naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_2.5TeV_parts',
+                'GtoWW25brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_2.5TeV_parts',
+                'GtoWW30naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_3.0TeV_parts',
+                'GtoWW30brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.0TeV_parts',
+                'GtoWW35naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_3.5TeV_parts',
+                'GtoWW35brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.5TeV_parts',
+                'GtoWW45naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_4.5TeV_parts',
+                'GtoWW45brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_4.5TeV_parts',
 	},
 }
 
