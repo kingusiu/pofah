fc923070da217507b965389271767f71e1502b93
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-10-01 15:11:15 +0200

    setting saved model dir in experiment path dict to eos drive

--- path_constants/experiment_dict.py
+++ path_constants/experiment_dict.py
@@ -1,7 +1,7 @@
 
 path_dict = {
 	
-	'model_dir' : 'models/$run$',
+	'model_dir' : '/eos/home-k/kiwoznia/data/VAE_models/$run$',
 	'analysis_base_dir_fig' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/fig/$strategy$/$quantile$',
 	'analysis_base_dir_bin_count' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/bin_count/$strategy$/$quantile$',
 	'model_analysis_base_dir': '/eos/home-k/kiwoznia/data/VAE_results/model_analysis/$run$',
