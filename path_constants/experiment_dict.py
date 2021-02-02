a4701ff401f4d4f2ced5e1bfe6ca96133179ff63
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-02-02 15:41:53 +0100

    updating path constants to qcd ext samples and qcd training and testing split for QR

--- path_constants/experiment_dict.py
+++ path_constants/experiment_dict.py
@@ -2,7 +2,7 @@
 path_dict = {
 	
 	'model_dir' : '/eos/home-k/kiwoznia/data/VAE_models/$run$',
-	'analysis_base_dir_fig' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/fig/$strategy$/$quantile$',
+	'analysis_base_dir_fig' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/fig/$strategy$',
 	'analysis_base_dir_bin_count' : '/eos/home-k/kiwoznia/data/VAE_results/bump_hunt_results/$run$/bin_count/$strategy$/$quantile$',
 	'model_analysis_base_dir': '/eos/home-k/kiwoznia/data/VAE_results/model_analysis/$run$',
 	'model_comparison_dir': '/eos/home-k/kiwoznia/data/VAE_results/model_analysis/$run1$_vs_$run2$'
--- path_constants/sample_dict_file_parts_reco.py
+++ path_constants/sample_dict_file_parts_reco.py
@@ -12,6 +12,7 @@ update_dict = {
                 'qcdSideReco': 'qcd_sqrtshatTeV_13TeV_PU40_NEW_sideband_parts',
                 'qcdSideExtReco' : 'qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_sideband_parts',
 		'qcdSigReco': 'qcd_sqrtshatTeV_13TeV_PU40_NEW_signalregion_parts',
+                'qcdSigExtReco': 'qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_signalregion_parts',
                 'GtoWW15naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_1.5TeV_NEW_parts',
                 'GtoWW15brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_1.5TeV_NEW_parts',
                 'GtoWW25naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_2.5TeV_NEW_parts',
@@ -20,6 +21,9 @@ update_dict = {
                 'GtoWW35brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_3.5TeV_NEW_parts',
                 'GtoWW45naReco': 'RSGraviton_WW_NARROW_13TeV_PU40_4.5TeV_NEW_parts',
                 'GtoWW45brReco': 'RSGraviton_WW_BROAD_13TeV_PU40_4.5TeV_NEW_parts',
+                # prepared train and test split data for QR training
+                'qcdSigRecoAllTrain': 'qcd_sqrtshatTeV_13TeV_PU40_NEW_ALL_Train_signalregion_parts',
+                'qcdSigRecoAllTest': 'qcd_sqrtshatTeV_13TeV_PU40_NEW_ALL_Test_signalregion_parts',
 	},
 }
 
